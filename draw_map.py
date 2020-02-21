7939a7e9a4bd1133885a4a471666ae58007658b0
Cheryn <cn397@cam.ac.uk>
2020-02-21 16:28:36 +0000

    added draw_map.py to draw obstacle map interactively

---
+++ draw_map.py
@@ -0,0 +1,109 @@
+# From https://stackoverflow.com/questions/30023763/how-to-make-an-interactive-2d-grid-in-a-window-in-python
+
+from tkinter import *
+import numpy as np
+
+class Cell():
+    FILLED_COLOR_BG = "green"
+    EMPTY_COLOR_BG = "white"
+    FILLED_COLOR_BORDER = "green"
+    EMPTY_COLOR_BORDER = "black"
+
+    def __init__(self, master, x, y, size):
+        """ Constructor of the object called by Cell(...) """
+        self.master = master
+        self.abs = x
+        self.ord = y
+        self.size = size
+        self.fill = False
+
+    def _switch(self):
+        """ Switch if the cell is filled or not. """
+        self.fill = not self.fill
+
+    def draw(self):
+        """ order to the cell to draw its representation on the canvas """
+        if self.master is not None :
+            fill = Cell.FILLED_COLOR_BG
+            outline = Cell.FILLED_COLOR_BORDER
+
+            if not self.fill:
+                fill = Cell.EMPTY_COLOR_BG
+                outline = Cell.EMPTY_COLOR_BORDER
+
+            xmin = self.abs * self.size
+            xmax = xmin + self.size
+            ymin = self.ord * self.size
+            ymax = ymin + self.size
+
+            self.master.create_rectangle(xmin, ymin, xmax, ymax, fill = fill, outline = outline)
+
+class CellGrid(Canvas):
+    def __init__(self,master, rowNumber, columnNumber, cellSize, *args, **kwargs):
+        Canvas.__init__(self, master, width = cellSize * columnNumber , height = cellSize * rowNumber, *args, **kwargs)
+
+        self.cellSize = cellSize
+        self.grid = []
+        for row in range(rowNumber):
+            line = []
+            for column in range(columnNumber):
+                line.append(Cell(self, column, row, cellSize))
+
+            self.grid.append(line)
+
+        #memorize the cells that have been modified to avoid many switching of state during mouse motion.
+        self.switched = []
+
+        self.matrix = np.zeros((rowNumber,columnNumber))
+
+        #bind click action
+        self.bind("<Button-1>", self.handleMouseClick)
+        #bind moving while clicking
+        self.bind("<B1-Motion>", self.handleMouseMotion)
+        #bind release button action - clear the memory of midified cells.
+        self.bind("<ButtonRelease-1>", lambda event: self.switched.clear())
+
+        self.draw()
+
+
+    def draw(self):
+        for row in self.grid:
+            for cell in row:
+                cell.draw()
+
+    def _eventCoords(self, event):
+        row = int(event.y / self.cellSize)
+        column = int(event.x / self.cellSize)
+        with open("map1.txt", "w") as file:
+            for line in self.matrix:
+                file.write(str(line) + " ")
+                file.write("\n")
+        return row, column
+
+    def handleMouseClick(self, event):
+        row, column = self._eventCoords(event)
+        cell = self.grid[row][column]
+        cell._switch()
+        cell.draw()
+        #add the cell to the list of cell switched during the click
+        self.switched.append(cell)
+        self.matrix[row][column] = 1
+
+    def handleMouseMotion(self, event):
+        row, column = self._eventCoords(event)
+        cell = self.grid[row][column]
+        self.matrix[row][column] = 1
+
+        if cell not in self.switched:
+            cell._switch()
+            cell.draw()
+            self.switched.append(cell)
+
+
+if __name__ == "__main__" :
+    app = Tk()
+
+    grid = CellGrid(app, 10, 10, 50)
+    grid.pack()
+
+    app.mainloop()
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
+# PSO adapted from: https://medium.com/analytics-vidhya/implementing-particle-swarm-optimization-pso-algorithm-in-python-9efc2eb179a6
+#
 import numpy as np
 import matplotlib.pyplot as plt
 import copy
 
-w = 0.7
-c1 = 2
-c2 = 2
+dim = 2
+gridsize = 100
 
 
 class Robot:
-    def __init__(self, dim, minx, maxx):
-        self.position = np.random.uniform(low=minx, high=maxx, size=dim)
-        self.velocity = np.random.uniform(low=-10, high=10, size=dim)
+    def __init__(self):
+        self.position = np.zeros(dim)
+        self.velocity = np.random.uniform(0, 10, dim)
 
         # particle best
         self.pbest_position = copy.deepcopy(self.position)
@@ -25,53 +26,52 @@ class Robot:
 
 # todo: obstacle map search space
 class Space:
-    def __init__(self, dims, target, target_error, n_robots):
+    def __init__(self, target, target_error, n_robots):
         self.target = target
         self.target_error = target_error
         self.n_robots = n_robots
         self.robots = []
 
         # global best
-        self.gbest_position = np.random.uniform(low=-100, high=100, size=dims)
+        self.gbest_position = np.random.uniform(0, gridsize, dim)
         self.gbest_value = float('inf')
 
     # todo: generate fitness based on distance
-    def fitness(self, robot, target):
-        return np.sum((target - robot.position)**2)**0.5
+    def fitness(self, robot):
+        return np.sum((self.target - robot.position)**2)**0.5
         # return robot.position[0] ** 2 + robot.position[1] ** 2 + 1
 
     def set_pbest(self):
         for robot in self.robots:
-            fitness_candidate = self.fitness(robot, self.target)
+            fitness_candidate = self.fitness(robot)
             if fitness_candidate < robot.pbest_value:
                 robot.pbest_value = copy.deepcopy(fitness_candidate)
                 robot.pbest_position = copy.deepcopy(robot.position)
 
     def set_gbest(self):
         for robot in self.robots:
-            best_fitness_candidate = self.fitness(robot, self.target)
+            best_fitness_candidate = self.fitness(robot)
             if best_fitness_candidate < self.gbest_value:
                 self.gbest_value = copy.deepcopy(best_fitness_candidate)
                 self.gbest_position = copy.deepcopy(robot.position)
 
     # todo: gridwise movements
-    def move_robots(self):
+    def move_robots(self, w, c1, c2):
         for robot in self.robots:
             robot.velocity = w * robot.velocity + \
-                           c1 * np.random.uniform(0,1) * (robot.pbest_position - robot.position) + \
-                           c2 * np.random.uniform(0,1) * (self.gbest_position - robot.position)
-
+                           c1 * np.random.uniform(0, 1) * (robot.pbest_position - robot.position) + \
+                           c2 * np.random.uniform(0, 1) * (self.gbest_position - robot.position)
             robot.move()
 
     def visualise(self):
+        plt.ion()
         plt.figure()
         plt.scatter(self.target[0], self.target[1], marker="o")
         for robot in self.robots:
-            plt.scatter(robot.position[0], robot.position[1],marker="x")
-        plt.xlim(-100,100)
-        plt.ylim(-100,100)
+            plt.scatter(robot.position[0], robot.position[1], marker="x")
+        plt.xlim(0, gridsize)
+        plt.ylim(0, gridsize)
         plt.show()
-
 
 def main():
     # Input
@@ -81,13 +81,13 @@ def main():
 
     n_iterations = 100
     target_error = 0.1
-    n_robots = 10
+    n_robots = 20
 
-    target = np.random.uniform(low=-100, high=100, size=2)
+    target = np.random.uniform(0, gridsize, dim)
     print("target location: ", target)
 
-    search_space = Space(2, target, target_error, n_robots)
-    robots_vector = [Robot(2, -100, 100) for _ in range(search_space.n_robots)]
+    search_space = Space(target, target_error, n_robots)
+    robots_vector = [Robot() for _ in range(search_space.n_robots)]
     search_space.robots = robots_vector
 
     iteration = 0
@@ -103,7 +103,7 @@ def main():
             print(search_space.gbest_position)
             break
 
-        search_space.move_robots()
+        search_space.move_robots(w=0.7, c1=2, c2=2)
         iteration += 1
 
     print("best solution is: ", search_space.gbest_position, "in n_iterations: ", iteration)
